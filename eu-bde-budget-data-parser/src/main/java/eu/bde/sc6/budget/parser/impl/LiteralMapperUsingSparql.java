7f6d5bd3e027d3072811290f16fda51b8c8d1b0d
syats <victor.mireles-chavez@semantic-web.com>
2017-05-26 17:07:39 +0200

    Sparql based literal mapper

--- eu-bde-budget-data-parser/src/main/java/eu/bde/sc6/budget/parser/impl/LiteralMapperUsingSparql.java
+++ eu-bde-budget-data-parser/src/main/java/eu/bde/sc6/budget/parser/impl/LiteralMapperUsingSparql.java
@@ -42,22 +42,21 @@ import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.sparql.SPARQLRepository;
 
 /**
- * A mapper that, given a list of RDF statements and a PP thesaurus, returns a list
- * of RDF statements where the literals have been substituted, when possible, for
- * URIs of concepts in the thesaurus.
- * 
- *  The input is scanned for statements of the form ?s P literal where P is in 
- * the objects set labelsToSubstitute (given in the constructor).
- * If the literal is found in the PP thesaurus in a statement of the form 
- *   ?S2 P2 literal   where P2 is from the objects matchingPredicates list, then
- * the original statement is substituted in the output for the statement ?s P ?S2
- * Otherwise, the original statement is left untouched.
+ * A mapper that, given a list of RDF statements and a PP thesaurus, returns a
+ * list of RDF statements where the literals have been substituted, when
+ * possible, for URIs of concepts in the thesaurus.
+ *
+ * The input is scanned for statements of the form ?s P literal where P is in
+ * the objects set labelsToSubstitute (given in the constructor). If the literal
+ * is found in the PP thesaurus in a statement of the form ?S2 P2 literal where
+ * P2 is from the objects matchingPredicates list, then the original statement
+ * is substituted in the output for the statement ?s P ?S2 Otherwise, the
+ * original statement is left untouched.
+ *
  * @author mireleschavezv
  */
-
-
 public class LiteralMapperUsingSparql {
-    
+
     private String PPURL;
     private Set<String> labels;
     private SPARQLRepository rep;
@@ -65,119 +64,133 @@ public class LiteralMapperUsingSparql {
     public Header authHeader;
     public LinkedList<String> matchingPredicates;
 
-    
-    public LiteralMapperUsingSparql(String PPHost, String PPProjectName, 
+    public LiteralMapperUsingSparql(String PPHost, String PPProjectName,
             String PPUser,
             String PPPass,
             String PPProject,
-            Set<String> labelsToSubstitute)  {
-        
-        this.PPURL = PPHost+"/PoolParty/sparql/"+PPProjectName;
-        this.labels = labelsToSubstitute;
-        this.theClient = new HttpClient();        
-        Credentials defaultcreds = new UsernamePasswordCredentials(PPUser, PPPass);        
+            Set<String> labelsToSubstitute) {
+
+        this.PPURL = PPHost + "/PoolParty/sparql/" + PPProjectName;
+
+        this.theClient = new HttpClient();
+        Credentials defaultcreds = new UsernamePasswordCredentials(PPUser, PPPass);
         this.theClient.getState().setCredentials(new AuthScope(PPHost, 80, AuthScope.ANY_REALM), defaultcreds);
-        
-        
+
         this.matchingPredicates = new LinkedList<>();
         this.matchingPredicates.add("http://www.w3.org/2004/02/skos/core#hiddenLabel");
         this.matchingPredicates.add("http://www.w3.org/2004/02/skos/core#prefLabel");
         this.matchingPredicates.add("http://www.w3.org/2004/02/skos/core#altLabel");
-                
+
+        if (labelsToSubstitute == null) {
+            HashSet<String> defaultLabels = new HashSet<>();
+            defaultLabels.add(DCTERMS.SUBJECT.toString());
+            this.labels = defaultLabels;
+        } else {
+            this.labels = labelsToSubstitute;
+        }
     }
-    
-    public void addMatchingPredicate(String s)
-    {
+
+    /*
+    For each LITERAL that could be substituted, the thesaurus will be queried
+    to find a statement of the form URI P LITERAL, where P is one of the 
+    matching Predicates. Use this method to add one more mathcing predcate.
+    */
+    public void addMatchingPredicate(String s) {
         this.matchingPredicates.add(s);
     }
-    
+
+    /*
+       If the input data contains a statement of the form X S LITERAL, where S
+    is one of the labels to substitute, substitution will be attempted. Use this
+    method to add one more label to substitute.
+     */
+    public void addLabelToSubstitute(String s) {
+        this.labels.add(s);
+    }
+
     /* Checks if a given string has correct parentrhesis
     From https://stackoverflow.com/a/16874430
-    */
-    public static boolean isParenthesisMatch(String str) {
+     */
+    private static boolean isParenthesisMatch(String str) {
 
-    Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<Character>();
 
-    char c;
-    for(int i=0; i < str.length(); i++) {
-        c = str.charAt(i);
+        char c;
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
 
-        if(c == '(')
-            stack.push(c);
-        else if(c == '{')
-            stack.push(c);
-        else if(c == ')')
-            if(stack.empty())
-                return false;
-            else if(stack.peek() == '(')
-                stack.pop();
-            else
-                return false;
-        else if(c == '}')
-            if(stack.empty())
-                return false;
-            else if(stack.peek() == '{')
-                stack.pop();
-            else
-                return false;
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == '{') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.empty()) {
+                    return false;
+                } else if (stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (stack.empty()) {
+                    return false;
+                } else if (stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
     }
-    return stack.empty();
-    }
-    
-    private String stringPreprocessing(String s)
-    {
-        String newObj = s.replace("\"","");
+
+    private String stringPreprocessing(String s) {
+        String newObj = s.replace("\"", "");
         newObj = newObj.replace("'", "");
         // If the stirng has non-matching parenthesis, they are removed
-        if (!this.isParenthesisMatch(newObj))
-        {
-            newObj = newObj.replace("(", "_");    
-            newObj = newObj.replace(")", "_");    
+        if (!this.isParenthesisMatch(newObj)) {
+            newObj = newObj.replace("(", "_");
+            newObj = newObj.replace(")", "_");
         }
         String objNorm = Normalizer.normalize(newObj, Normalizer.Form.NFD);
         String objPure = StringUtils.stripAccents(objNorm.toLowerCase());
         return (objPure);
     }
-    
+
     /*
      For a statement to be mappable it must contain a literal in the object, and
     the predicate must be one of self.labels
-    */
-    private Boolean statementMightBeMappable(Statement s)
-    {
+     */
+    private Boolean statementMightBeMappable(Statement s) {
         Value obj = s.getObject();
-        if (obj.getClass().getCanonicalName()!=LiteralImpl.class.getCanonicalName() )        
+        if (obj.getClass().getCanonicalName() != LiteralImpl.class.getCanonicalName()) {
             return false;
-        
+        }
+
         String predicate = s.getPredicate().toString();
-        if (this.labels.contains(predicate))
+        if (this.labels.contains(predicate)) {
             return true;
-        
+        }
+
         return false;
-        
-        
+
     }
-    
-    public List<Statement> map(List<Statement> data)
-    {
-        Set <String> literals = this.getAllLiterals(data);
-        System.out.println((new Integer(literals.size())).toString()+ " different literals found" );
-        Map<String,URIImpl> LiteralUriMap = this.computeLiteralUriMapping(literals);
+
+    public List<Statement> map(List<Statement> data) {
+        Set<String> literals = this.getAllLiterals(data);
+        System.out.println((new Integer(literals.size())).toString() + " different literals found");
+        Map<String, URIImpl> LiteralUriMap = this.computeLiteralUriMapping(literals);
         HashSet<URIImpl> SetOfUsedURIs = new HashSet<>();
-        
-        System.out.println((new Integer(LiteralUriMap.size())).toString()+ " literals Mapped" );
-        
-        
-        List <Statement> result = new LinkedList<Statement>();
-        for (Statement s : data)
-        {
+
+        System.out.println((new Integer(LiteralUriMap.size())).toString() + " literals Mapped");
+
+        List<Statement> result = new LinkedList<Statement>();
+        for (Statement s : data) {
             Statement sNew = s;
-            if (this.statementMightBeMappable(s))
-            {
-                Value obj = s.getObject();            
+            if (this.statementMightBeMappable(s)) {
+                Value obj = s.getObject();
                 String objStr = stringPreprocessing(obj.toString());
-                if (LiteralUriMap.containsKey(objStr))
-                {
+                if (LiteralUriMap.containsKey(objStr)) {
                     URIImpl mappedUri = LiteralUriMap.get(objStr);
                     sNew = new StatementImpl(s.getSubject(), s.getPredicate(), mappedUri);
                     SetOfUsedURIs.add(mappedUri);
@@ -186,77 +199,68 @@ public class LiteralMapperUsingSparql {
             result.add(sNew);
         }
         return (result);
-        
-                
+
     }
-    
-    private Set<String> getAllLiterals(List<Statement> data)
-    {
+
+    public Set<String> getAllLiterals(List<Statement> data) {
         HashSet<String> listOfLiterals = new HashSet<>();
-        for (Statement s : data)
-        {
+        for (Statement s : data) {
             Statement sNew = s;
-            if (this.statementMightBeMappable(s))
-            {
+            if (this.statementMightBeMappable(s)) {
                 String cleanLiteral = this.stringPreprocessing(s.getObject().toString());
                 listOfLiterals.add(cleanLiteral);
             }
         }
         return listOfLiterals;
     }
-    
-    private Map<String,URIImpl> computeLiteralUriMapping(Set <String> literals)
-    {
-        TreeMap<String,URIImpl>  theMap = new TreeMap<>();
-        for (String literal : literals)
-        {
+
+    private Map<String, URIImpl> computeLiteralUriMapping(Set<String> literals) {
+        TreeMap<String, URIImpl> theMap = new TreeMap<>();
+        for (String literal : literals) {
             URIImpl uri = this.findPossibleURI(literal);
-            if (uri!=null)
-            {
+            if (uri != null) {
                 theMap.put(literal, uri);
             }
         }
         return theMap;
     }
 
-    private URIImpl findPossibleURI(String literal)  
-    {
-                
+    private URIImpl findPossibleURI(String literal) {
+
         String query = "SELECT ?X\n WHERE {";
-        for (int predN=0;predN<this.matchingPredicates.size();predN++)
-        {
-            String pred = this.matchingPredicates.get(predN);        
-            query+="\n{?X <"+pred+"> ?o. \n FILTER (regex(?o, '^"+literal+"$') )}\n";
-            if (predN < this.matchingPredicates.size()-1)
-            {
-                query+="UNION";
+        for (int predN = 0; predN < this.matchingPredicates.size(); predN++) {
+            String pred = this.matchingPredicates.get(predN);
+            query += "\n{?X <" + pred + "> ?o. \n FILTER (regex(?o, '^" + literal + "$') )}\n";
+            if (predN < this.matchingPredicates.size() - 1) {
+                query += "UNION";
             }
-            
-        }        
-        query+="}";
+
+        }
+        query += "}";
         try {
-        String encodedQuery = URLEncoder.encode(query, "UTF-8");
-        HttpMethod method = new GetMethod(this.PPURL+"?query="+encodedQuery+"&content-type=application%2Fjson");        
-        
+            String encodedQuery = URLEncoder.encode(query, "UTF-8");
+            HttpMethod method = new GetMethod(this.PPURL + "?query=" + encodedQuery + "&content-type=application%2Fjson");
+
             this.theClient.executeMethod(method);
             String S = method.getResponseBodyAsString();
             JSONObject jsonReponse = new JSONObject(S);
             JSONObject res = jsonReponse.getJSONObject("results");
             JSONArray bindings = res.getJSONArray("bindings");
-            if (bindings.length() < 1)
+            if (bindings.length() < 1) {
                 return null;
+            }
             JSONObject first = bindings.getJSONObject(0).getJSONObject("X");
             String val = first.getString("value");
 
-            URIImpl uri = new URIImpl(val);           
-            
+            URIImpl uri = new URIImpl(val);
+
             return uri;
-            
-        } catch (Exception  ex) {
+
+        } catch (Exception ex) {
             Logger.getLogger(LiteralMapperUsingSparql.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("\nOffendingQuery:-------}\n"+query+"\n\n");
+            System.out.println("\nOffendingQuery:-------}\n" + query + "\n\n");
             return (null);
-        }        
+        }
     }
-    
+
 }
